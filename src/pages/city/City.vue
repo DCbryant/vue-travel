d53d37a95a33a91a8a83265b6b104c7993aaf83e
dcbryant <dcbryant753@gmail.com>
2018-05-14 22:47:19 +0800

    finish city css

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,23 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alpahbet></city-alpahbet>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlpahbet from './components/Alphabet'
 export default {
   name: 'city',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlpahbet
   }
 }
 </script>
---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="list">
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+    <div class="item">A</div>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlpahbet'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+    .list
+      position: absolute
+      top: 1.58rem
+      right: 0
+      bottom: 0
+      width: .4rem
+      display: flex
+      flex-direction: column
+      justify-content: center
+      .item
+        color: #000
+        text-align: center
+        line-height: .4rem
+        color: $bgColor
+</style>
